dfaa0255c665ff07b493d4bc6373678983c5c624
nenahui <kanatraccoon@gmail.com>
2024-07-27 13:41:47 +0600

    A couple of improvements and improvements to the site header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,13 @@
-import { HomeOutlined, OrderedListOutlined } from '@ant-design/icons';
+import { OrderedListOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import { Flex, Menu, type MenuProps, Typography } from 'antd';
 import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const { Text } = Typography;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
 const items: MenuItem[] = [
-  {
-    label: (
-      <Link style={{ fontSize: 12 }} to={'/'}>
-        Home
-      </Link>
-    ),
-    key: 'home',
-    icon: <HomeOutlined />,
-  },
   {
     label: (
       <Link style={{ fontSize: 12 }} to={'/categories'}>
@@ -26,19 +17,36 @@ const items: MenuItem[] = [
     key: 'categories',
     icon: <OrderedListOutlined />,
   },
+  {
+    label: (
+      <Link style={{ fontSize: 12 }} to={'/add'}>
+        Add
+      </Link>
+    ),
+    key: 'add',
+    icon: <PlusCircleOutlined />,
+  },
 ];
 
 export const Header = () => {
+  const navigate = useNavigate();
   const { pathname: location } = useLocation();
-  const [current, setCurrent] = useState(location === '/' ? 'home' : location.slice(1));
+  const [current, setCurrent] = useState(location.slice(1));
 
   const onClick: MenuProps['onClick'] = (e) => {
     setCurrent(e.key);
   };
 
+  const navigateHome = () => {
+    setCurrent('');
+    navigate('/');
+  };
+
   return (
     <Flex justify={'space-between'} align={'center'}>
-      <Text className={'fz-18'}>Finance Tracker</Text>
+      <Text className={'fz-22 logo'} onClick={navigateHome}>
+        Finance Tracker
+      </Text>
       <Menu onClick={onClick} selectedKeys={[current]} mode='horizontal' items={items} />
     </Flex>
   );
